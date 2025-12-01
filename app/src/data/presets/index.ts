43bc4e06a2a4ff2534c09f48ae767ba44af6950e
Derek L <derekluciani@gmail.com>
2025-12-01 08:45:17 -0500

    updated patch presets

--- app/src/data/presets/index.ts
+++ app/src/data/presets/index.ts
@@ -1,11 +1,12 @@
-import angryLemmings from './Angry Lemmings.json';
 import basicPatch from './Basic Patch.json';
-import drone from './Drone.json';
+import drive from './Drive.json';
+import angryLemmings from './Angry Lemmings.json';
+// import drone from './Drone.json';
 import onTheRun from './On The Run.json';
 
 export const defaultPresets = [
     basicPatch,
+    drive,
     angryLemmings,
-    drone,
     onTheRun
 ];
